6954625821aedccf3af76ed8e83fd9bcc35f6b30
alexeyborisov19-sys <alexeyborisov19@gmail.com>
2026-08-25 14:11:10 +0300

    perf: remove blocking site preloader

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -3,7 +3,6 @@ import { Analytics } from "@/components/Analytics";
 import { CookieConsent } from "@/components/CookieConsent";
 import { EngineeringAssistantLauncher } from "@/components/EngineeringAssistantLauncher";
 import { JsonLd } from "@/components/JsonLd";
-import { SitePreloader } from "@/components/SitePreloader";
 import { organizationSchema, websiteSchema } from "@/lib/schema";
 import { siteConfig } from "@/lib/site";
 
@@ -69,7 +68,6 @@ export default function PublicLayout({ children }: Readonly<{ children: React.Re
     <>
       <JsonLd data={[organizationSchema(), websiteSchema()]} />
       <Analytics />
-      <SitePreloader />
       {children}
       <EngineeringAssistantLauncher />
       <CookieConsent />
