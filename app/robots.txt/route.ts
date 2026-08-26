7fb09bec2a085e38e01466844739f33357fc9469
alexeyborisov19-sys <alexeyborisov19@gmail.com>
2026-08-26 14:17:43 +0300

    seo: remove obsolete robots Host directive

--- app/robots.txt/route.ts
+++ app/robots.txt/route.ts
@@ -9,7 +9,6 @@ Disallow: /api/
 Disallow: /internal/
 Clean-param: utm_source&utm_medium&utm_campaign&utm_term&utm_content&gclid&yclid&fbclid&msclkid&gad_source&gbraid&wbraid&_openstat&_ym_status-check
 
-Host: ${siteConfig.url}
 Sitemap: ${siteConfig.url}/sitemap.xml
 Sitemap: ${siteConfig.url}/sitemap-images.xml
 `;
