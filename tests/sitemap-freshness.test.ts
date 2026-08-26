11192336220bfc06834c370cb77ce93f4054e068
alexeyborisov19-sys <alexeyborisov19@gmail.com>
2026-08-26 14:24:31 +0300

    test: guard company and contacts sitemap freshness

--- tests/sitemap-freshness.test.ts
+++ tests/sitemap-freshness.test.ts
@@ -3,6 +3,8 @@ import test from "node:test";
 import sitemap from "@/app/sitemap";
 
 const expectedFreshness = new Map([
+  ["https://www.steelprodukt.ru/company", Date.parse("2026-08-25T15:10:21.000Z")],
+  ["https://www.steelprodukt.ru/contacts", Date.parse("2026-08-25T17:32:36.000Z")],
   ["https://www.steelprodukt.ru/production", Date.parse("2026-08-25T21:24:08.000Z")],
   ["https://www.steelprodukt.ru/products", Date.parse("2026-08-25T15:45:53.000Z")],
 ]);
