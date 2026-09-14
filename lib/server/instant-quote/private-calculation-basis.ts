fbb63a41f140840670d55dab0d1e83574ef2c99a
alexeyborisov19-sys <alexeyborisov19@gmail.com>
2026-09-14 16:15:00 +0300

    Validate additional confidential production rates

--- lib/server/instant-quote/private-calculation-basis.ts
+++ lib/server/instant-quote/private-calculation-basis.ts
@@ -161,6 +161,9 @@ function parseBasis(value: unknown): PrivateCalculationBasis {
       bendRubEach: rate(rateBookRaw.bendRubEach, "bendRubEach"),
       weldRubPerM: rate(rateBookRaw.weldRubPerM, "weldRubPerM"),
       powderRubPerM2: rate(rateBookRaw.powderRubPerM2, "powderRubPerM2"),
+      assemblyRubPerHour: rate(rateBookRaw.assemblyRubPerHour, "assemblyRubPerHour"),
+      surfacePreparationRubPerM2: rate(rateBookRaw.surfacePreparationRubPerM2, "surfacePreparationRubPerM2"),
+      packagingRubEach: rate(rateBookRaw.packagingRubEach, "packagingRubEach"),
     },
     materialPriceSnapshots,
   };
