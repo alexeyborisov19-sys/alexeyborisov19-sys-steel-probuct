df17900abbb789151f1a693fa9a4a0b561543323
alexeyborisov19-sys <alexeyborisov19@gmail.com>
2026-09-17 23:19:01 +0300

    Подключить голосовой профиль ИИ-инженера

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import "./globals.css";
 import "./interaction-accessibility.css";
+import { EngineeringVoiceProfile } from "@/components/EngineeringVoiceProfile";
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ru">
-      <body>{children}</body>
+      <body>
+        <EngineeringVoiceProfile />
+        {children}
+      </body>
     </html>
   );
 }
